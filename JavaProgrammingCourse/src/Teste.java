f38e5760c5cea2291a848ba90ace83f26cacc825
Eduardo Oliveira <theeduardooliveira@outlook.com>
2023-10-10 15:29:31 +0100

    Initial commit

---
+++ JavaProgrammingCourse/src/Teste.java
@@ -0,0 +1,15 @@
+import java.util.Locale;
+
+public class Teste {
+    public static void main(String[] args){
+        
+        double A = 2549885.398784525;
+        System.out.println("Hello World!");
+        System.out.println("Bom dia.");
+        System.out.println(A);
+        System.out.printf("%.2f\n",A);
+        System.out.printf("%.4f\n", A);
+        
+
+    }
+}
